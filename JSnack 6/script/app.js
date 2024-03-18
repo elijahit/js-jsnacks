4af306438a1e4ce627c9414f97dfb7e3b3be0a1e
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-18 22:45:18 +0100

    END: JSnack 6

---
+++ JSnack 6/script/app.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 11)
+let userNumber = +prompt("Inserici un numero");
+
+if(randomNumber === userNumber) {
+  console.log("YOU WON");
+} else {
+  console.log("YOU W.. LOSE! 🧔")
+}
