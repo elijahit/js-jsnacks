7ebad07381be442a62926e2797ac838ad34c7740
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-03-18 22:18:50 +0100

    END: JSnack 2

---
+++ JSnack 2/script/app.js
@@ -0,0 +1,4 @@
+let strA = prompt("Inserisci una parola"); //String
+let strB = prompt("Inserisci una parola"); //String
+
+strA.length > strB.length ? console.log(strB, strA) : console.log(strA, strB);
